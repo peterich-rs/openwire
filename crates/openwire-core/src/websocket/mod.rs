759ce85027b45a588477ba6486e6f02b456dc0eb
fannnzhang <fannnzhang@gmail.com>
2026-04-30 21:36:18 +0800

    feat(websocket): add error types

--- crates/openwire-core/src/websocket/mod.rs
+++ crates/openwire-core/src/websocket/mod.rs
@@ -2,4 +2,5 @@ pub mod engine;
 pub mod error;
 pub mod message;
 
+pub use error::{HandshakeFailure, TimeoutKind, WebSocketEngineError, WebSocketError};
 pub use message::{CloseInitiator, Message, MessageKind};
